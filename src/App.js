94c09d373d9cdca4f733565ca59774441f72027f
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-03-03 16:40:20 +0000

    set up

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Events from './components/Events/index'
+
+import './App.css'
+
+const App = () => <Events />
+
+export default App
---
+++ src/components/EventItem/index.js
@@ -0,0 +1,6 @@
+// Write your code here
+import './index.css'
+
+const EventItem = () => <h1>EventItem</h1>
+
+export default EventItem
